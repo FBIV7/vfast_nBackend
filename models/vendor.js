311dabf8815a5cc7d5f2d8762eda89d29534361a
aman2698 <developers@mac9.in>
2021-03-05 15:59:39 +0530

    ds

--- models/vendor.js
+++ models/vendor.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const VendorSchema = mongoose.Schema({
   name: String,
-  aggrement: { type: Boolean },
+  aggrement: { type: String },
   startDate: { type: String },
   endDate: { type: String },
   price:[{ locationName : String, areaType :String, cost:Number  }] ,

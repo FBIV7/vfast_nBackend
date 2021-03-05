7193819ee9b8ac8dbb83a389c87ffdd511cc8c6f
aman2698 <developers@mac9.in>
2021-03-05 16:19:50 +0530

    frd

--- models/vendor.js
+++ models/vendor.js
@@ -3,10 +3,9 @@ const Schema = mongoose.Schema;
 
 const VendorSchema = mongoose.Schema({
   name: String,
-  aggrement: { type: String },
+  agreement: { type: String },
   startDate: { type: String },
   endDate: { type: String },
-  price:[{ locationName : String, areaType :String, cost:Number  }] ,
-
+  price: [{ locationName: String, areaType: String, cost: Number }],
 });
 module.exports = mongoose.model("Vendor", VendorSchema);
